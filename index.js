41a7a96f6bc9ecc939af4860b04bdf214df64012
dragoocho <deagoocho@gmail.com>
2021-02-23 15:40:37 +0900

    서버 테스트 commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("hello");
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+function handleListening() {
+  console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+function handleHome(req, res) {
+  res.send("my home");
+}
+
+function handleProfile(req, res) {
+  res.send("my profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
